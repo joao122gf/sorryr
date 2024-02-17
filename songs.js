84ddf1098fc52d774dedc329774232b9b78a9b4b
joao122gf <160354718+joao122gf@users.noreply.github.com>
2024-02-17 19:34:55 -0300

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    src: "./src/Shake It Bololo ft. Classics of MPB.mp3",
+    name: "Shake it",
+  },
+  {
+    src: "./src/a360ytmp3.com_320kbps-tamo-ai-na-atividade.mp3",
+    name: "Tamo ai na atividade",
+  },
+  {
+    src: "./src/Lenny Kravitz - Fly Away (Official Music Video).mp3",
+    name: "Fly Away",
+  },
+  {
+    src: "./src/Red Hot Chili Peppers - Tell Me Baby (mp3cut.net).mp3",
+    name: "Tell Me Baby",
+  },
+];
